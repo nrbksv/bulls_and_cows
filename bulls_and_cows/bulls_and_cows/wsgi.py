9eeb3aee6bcd3b52bd36d74f54e40a6da4ef9ea8
nrbk <nrbksv@gmail.com>
2021-02-21 19:20:02 +0600

    Initial commit.

---
+++ bulls_and_cows/bulls_and_cows/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bulls_and_cows project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bulls_and_cows.settings')
+
+application = get_wsgi_application()
